232ec0f7ba388a44d61d88fa3e91ac63711ce7d9
israelsantana01 <israelofi2@gmail.com>
2022-01-20 15:31:05 -0300

    setup prisma and integrate with graphql

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,6 @@
-const { gql, ApolloServer } = require("apollo-server");
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World!'
-  }
-};
+const { ApolloServer } = require("apollo-server");
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/schemas");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen();
