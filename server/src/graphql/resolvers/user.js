97521f61ee5ead529e5d79a0f6b1f21e305bf0ed
israelsantana01 <israelofi2@gmail.com>
2022-01-20 15:46:26 -0300

    setup schemas and resolvers

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -1,14 +1,33 @@
-function getUsers(params) {
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
+async function getUsers() {
+  const users = await prisma.user.findMany();
+  return users;
 }
 
-function getUser(params) {
+async function getUser(_, { email }) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+  return user;
+}
 
+async function createUser(_, { data }) {
+  const user = await prisma.user.create({
+    data
+  });
+  return user;
 }
 
 module.exports = {
   Query: {
     users: getUsers,
     user: getUser
+  },
+  Mutation: {
+    createUser: createUser
   }
 };
