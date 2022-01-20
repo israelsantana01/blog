e7509558063332ed4d3d8ee67753b073edd6780a
israelsantana01 <israelofi2@gmail.com>
2022-01-20 16:57:14 -0300

    setup schema and resolver for post and user entities

--- server/src/graphql/resolvers/post.js
+++ server/src/graphql/resolvers/post.js
@@ -1,14 +1,33 @@
-function getPosts(params) {
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
+async function getPosts(params) {
+  const posts = await prisma.post.findMany();
+  return posts;
 }
 
-function getPost(params) {
+async function getPost(_, { id }) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id
+    }
+  });
+  return post;
+}
 
+async function createPost(_, { data }) {
+  const post = await prisma.post.create({
+    data
+  });
+  return post;
 }
 
 module.exports = {
   Query: {
     posts: getPosts,
     post: getPost
+  },
+  Mutation: {
+    createPost: createPost
   }
 };
