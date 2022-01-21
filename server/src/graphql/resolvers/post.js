e39dbd2bfa67ea8fa09730e2bc4e33e31a06a2fa
israelsantana01 <israelofi2@gmail.com>
2022-01-21 10:29:22 -0300

    finish server development

--- server/src/graphql/resolvers/post.js
+++ server/src/graphql/resolvers/post.js
@@ -2,7 +2,9 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function getPosts(params) {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    include: { author: true }
+  });
   return posts;
 }
 
@@ -10,7 +12,8 @@ async function getPost(_, { id }) {
   const post = await prisma.post.findUnique({
     where: {
       id
-    }
+    },
+    include: { author: true }
   });
   return post;
 }
